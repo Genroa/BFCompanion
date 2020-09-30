d8066d7343b48b2ba95da0bcada90f5def655d5c
Genroa <genroa@hotmail.fr>
2020-09-30 15:36:19 +0200

    cleaned useless helper in heroes

--- src/helpers/heroes.js
+++ src/helpers/heroes.js
@@ -10,17 +10,6 @@ export const heroesBackgrounds = {
 	ratling_twins: { backgroundPosition: 'calc(80% + 100px) 35%' }
 }
 
-export const heroesFightBackgrounds = {
-	pious_vorne: { backgroundPosition: 'calc(80% + 200px) 20%' },
-	amallyn_shadowguide: { backgroundPosition: 'calc(80% + 100px) 31%' },
-	dahyak_grekh: { backgroundPosition: 'calc(80% + 100px) 15%' },
-	'ur-025': { backgroundPosition: 'calc(80% + 90px) 30%' },
-	janus_draik: { backgroundPosition: 'calc(80% + 100px) 13%' },
-	taddeus_the_purifier: { backgroundPosition: 'calc(80% + 150px) 20%' },
-	espern_locarno: { backgroundPosition: 'calc(80% + 100px) 0%' },
-	ratling_twins: { backgroundPosition: 'calc(80% + 100px) 35%' }
-}
-
 export const heroesList = [
 	{
 		id: 'janus_draik',
